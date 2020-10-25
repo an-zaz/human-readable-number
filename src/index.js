d5f9c6d070a028fbb13bc92d0d55b67c64faa16a
Nastya <an_zazerskaya@mail.ru>
2020-10-25 20:03:38 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
+const NUMBERS = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+};
+
+const HUNDRED = 'hundred';
+
 module.exports = function toReadable (number) {
-  
-}
+    if (number === 0) {
+        return NUMBERS[0];
+    }
+    let array = `${number}`.split('');
+    let currentDigitPosition = array.length;
+    let result = '';
+
+    for (let digit of array) {
+        if (currentDigitPosition === 3) {
+            result += `${NUMBERS[digit]} ${HUNDRED} `;
+        } else if (currentDigitPosition === 2 && +digit === 1) {
+            result += `${NUMBERS[digit + array[array.length - 1]]} `;
+            break;
+        } else if (currentDigitPosition === 2 && +digit) {
+            result += `${NUMBERS[digit * 10]} `;
+        } else if (currentDigitPosition === 1 && +digit) {
+            result += NUMBERS[digit];
+        }
+        currentDigitPosition--;
+    }
+    return result.trim();
+};
